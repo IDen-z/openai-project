4e59a5696a7a270639136cc1ad86284fed66de23
Secrets <zmzxdal@foxmail.com>
2023-12-15 17:35:08 +0800

    FEAT:gpt模型sdk引入

---
+++ src/main/java/com/zmz/chatgpt/interfaces/WeiXinPortalController.java
@@ -0,0 +1,58 @@
+package com.zmz.chatgpt.interfaces;
+
+import com.zmz.chatgpt.session.Configuration;
+import com.zmz.chatgpt.session.OpenAiSession;
+import com.zmz.chatgpt.session.OpenAiSessionFactory;
+import com.zmz.chatgpt.session.defaults.DefaultOpenAiSessionFactory;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class WeiXinPortalController {
+
+    private OpenAiSession openAiSession;
+
+    public WeiXinPortalController() {
+        // 1. 配置文件
+        Configuration configuration = new Configuration();
+        configuration.setApiHost("https://open.bigmodel.cn/");
+        configuration.setApiSecretKey("c8ebb130fc7fcf11c6fb826f6b55c412.dFYCXMJs3P6QDotr");
+        configuration.setLevel(HttpLoggingInterceptor.Level.BODY);
+        // 2. 会话工厂
+        OpenAiSessionFactory factory = new DefaultOpenAiSessionFactory(configuration);
+        // 3. 开启会话
+        this.openAiSession = factory.openSession();
+        log.info("初始化 openAiSession");
+    }
+
+    @GetMapping(produces = "text/plain;charset=utf-8")
+    public String validate(@PathVariable String appid,
+                           @RequestParam(value = "signature", required = false) String signature,
+                           @RequestParam(value = "timestamp", required = false) String timestamp,
+                           @RequestParam(value = "nonce", required = false) String nonce,
+                           @RequestParam(value = "echostr", required = false) String echostr) {
+        try {
+            log.info("微信公众号验签信息{}开始 [{}, {}, {}, {}]", appid, signature, timestamp, nonce, echostr);
+            if (StringUtils.isAnyBlank(signature, timestamp, nonce, echostr)) {
+                throw new IllegalArgumentException("请求参数非法，请核实!");
+            }
+            boolean check = weiXinValidateService.checkSign(signature, timestamp, nonce);
+            log.info("微信公众号验签信息{}完成 check：{}", appid, check);
+            if (!check) {
+                return null;
+            }
+            return echostr;
+        } catch (Exception e) {
+            log.error("微信公众号验签信息{}失败 [{}, {}, {}, {}]", appid, signature, timestamp, nonce, echostr, e);
+            return null;
+        }
+    }
+
+
+}
